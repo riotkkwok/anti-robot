b5be40466b976fd4d4a5093e6fc0bcf4b6d0c04a
guozijian <guozijian@yy.com>
2020-04-17 20:51:31 +0800

    init: 项目初始化

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        "anti-robot.min": "./src/index.ts",
+    },
+    output: {
+        path: __dirname,
+        filename: "build/[name].js",
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: path.join(__dirname, 'src'),
+                use: {
+                  loader: 'ts-loader',
+                  // options: {
+                  //   presets: ['es2015']
+                  // }
+                }
+            }
+        ],
+    },
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          include: /\.min\.js$/,
+        }),
+      ],
+    },
+}
